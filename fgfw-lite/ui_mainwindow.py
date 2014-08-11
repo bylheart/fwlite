012d96322726141fd1fb093bc49d75db9e454bdf
v3aqb <sgzz.cj@gmail.com>
2014-08-11 10:17:58 +0800

    update

--- fgfw-lite/ui_mainwindow.py
+++ fgfw-lite/ui_mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file './fgfw-lite/ui\mainwindow.ui'
 #
-# Created: Thu Aug 07 16:18:00 2014
+# Created: Mon Aug 11 10:17:19 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
