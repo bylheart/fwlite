70ed44e65ce32fa07ecd90b3bc10cd4f1bcd2a06
v3aqb <sgzz.cj@gmail.com>
2014-09-16 13:09:21 +0800

    fix py3

--- fgfw-lite/fgfw-lite.py
+++ fgfw-lite/fgfw-lite.py
@@ -466,7 +466,7 @@ class ProxyHandler(HTTPRequestHandler):
         except NetWorkIOError as e:
             return self.on_GET_Error(e)
         header_data = b''.join(header_data)
-        response_header = email.message_from_string(header_data)
+        response_header = email.message_from_string(str(header_data))
         conntype = response_header.get('Connection', "")
         if protocol_version >= b"HTTP/1.1":
             self.close_connection = conntype.lower() == 'close'
