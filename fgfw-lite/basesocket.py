e65cfe061f5ed9c484d8946087f3b6bc048dd303
v3aqb <sgzz.cj@gmail.com>
2015-02-17 13:34:40 +0800

    add basesocket.py

---
+++ fgfw-lite/basesocket.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding:utf-8
+import io
+import errno
+import socket
+
+
+class basesocket(object):
+    def __init__(self):
+        self._rbuffer = io.BytesIO()
+        self._sock = None
+
+    def readline(self, size=-1):
+        buf = self._rbuffer
+        buf.seek(0, 2)  # seek end
+        if buf.tell() > 0:
+            # check if we already have it in our buffer
+            buf.seek(0)
+            bline = buf.readline(size)
+            if bline.endswith('\n') or len(bline) == size:
+                self._rbuffer = io.BytesIO()
+                self._rbuffer.write(buf.read())
+                return bline
+            del bline
+        if size < 0:
+            # Read until \n or EOF, whichever comes first
+            buf.seek(0, 2)  # seek end
+            self._rbuffer = io.BytesIO()  # reset _rbuf.  we consume it via buf.
+            while True:
+                try:
+                    data = self.recv(self.bufsize)
+                except socket.error as e:
+                    if e.args[0] == errno.EINTR:
+                        continue
+                    raise
+                if not data:
+                    break
+                nl = data.find(b'\n')
+                if nl >= 0:
+                    nl += 1
+                    buf.write(data[:nl])
+                    self._rbuffer.write(data[nl:])
+                    break
+                buf.write(data)
+            del data
+            return buf.getvalue()
+        else:
+            # Read until size bytes or \n or EOF seen, whichever comes first
+            buf.seek(0, 2)  # seek end
+            buf_len = buf.tell()
+            if buf_len >= size:
+                buf.seek(0)
+                rv = buf.read(size)
+                self._rbuffer = io.BytesIO()
+                self._rbuffer.write(buf.read())
+                return rv
+            self._rbuffer = io.BytesIO()  # reset _rbuf.  we consume it via buf.
+            while True:
+                try:
+                    data = self.recv(self.bufsize)
+                except socket.error as e:
+                    if e.args[0] == errno.EINTR:
+                        continue
+                    raise
+                if not data:
+                    break
+                left = size - buf_len
+                # did we just receive a newline?
+                nl = data.find(b'\n', 0, left)
+                if nl >= 0:
+                    nl += 1
+                    # save the excess data to _rbuf
+                    self._rbuffer.write(data[nl:])
+                    if buf_len:
+                        buf.write(data[:nl])
+                        break
+                    else:
+                        # Shortcut.  Avoid data copy through buf when returning
+                        # a substring of our first recv().
+                        return data[:nl]
+                n = len(data)
+                if n == size and not buf_len:
+                    # Shortcut.  Avoid data copy through buf when
+                    # returning exactly all of our first recv().
+                    return data
+                if n >= left:
+                    buf.write(data[:left])
+                    self._rbuffer.write(data[left:])
+                    break
+                buf.write(data)
+                buf_len += n
+                # assert buf_len == buf.tell()
+            return buf.getvalue()
+
+    def close(self):
+        if self._sock:
+            self._sock.close()
+
+    def __del__(self):
+        self.close()
+
+    def settimeout(self, timeout):
+        self._sock.settimeout(timeout)
--- fgfw-lite/hxsocks.py
+++ fgfw-lite/hxsocks.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # coding:utf-8
-import socket
 import struct
 import encrypt
-import errno
 import io
 import time
 import hashlib
@@ -15,6 +13,7 @@ try:
     import urllib.parse as urlparse
 except ImportError:
     import urlparse
+from basesocket import basesocket
 from parent_proxy import ParentProxy
 from dh import DH
 
@@ -23,10 +22,11 @@ keys = {}
 newkey_lock = defaultdict(RLock)
 
 
-class hxssocket(object):
+class hxssocket(basesocket):
     bufsize = 8192
 
     def __init__(self, hxsServer=None, ctimeout=1, parentproxy=None, iplist=None):
+        basesocket.__init__(self)
         if hxsServer and not isinstance(hxsServer, ParentProxy):
             hxsServer = ParentProxy(hxsServer, hxsServer)
         self.hxsServer = hxsServer
@@ -37,13 +37,11 @@ class hxssocket(object):
         if self.hxsServer:
             self.PSK = urlparse.parse_qs(self.hxsServer.parse.query).get('PSK', [''])[0]
             self.method = urlparse.parse_qs(self.hxsServer.parse.query).get('method', [''])[0] or default_method
-        self._sock = None
         self.cipher = None
         self.connected = 0
         # value: 0: request not sent
         #        1: request sent, no server response received
         #        2: server response received
-        self._rbuffer = io.BytesIO()
 
     def connect(self, address):
         self.getKey()
@@ -119,95 +117,6 @@ class hxssocket(object):
         else:
             self._sock.sendall(self.cipher.encrypt(data))
 
-    def readline(self, size=-1):
-        buf = self._rbuffer
-        buf.seek(0, 2)  # seek end
-        if buf.tell() > 0:
-            # check if we already have it in our buffer
-            buf.seek(0)
-            bline = buf.readline(size)
-            if bline.endswith('\n') or len(bline) == size:
-                self._rbuffer = io.BytesIO()
-                self._rbuffer.write(buf.read())
-                return bline
-            del bline
-        if size < 0:
-            # Read until \n or EOF, whichever comes first
-            buf.seek(0, 2)  # seek end
-            self._rbuffer = io.BytesIO()  # reset _rbuf.  we consume it via buf.
-            while True:
-                try:
-                    data = self.recv(self.bufsize)
-                except socket.error as e:
-                    if e.args[0] == errno.EINTR:
-                        continue
-                    raise
-                if not data:
-                    break
-                nl = data.find(b'\n')
-                if nl >= 0:
-                    nl += 1
-                    buf.write(data[:nl])
-                    self._rbuffer.write(data[nl:])
-                    break
-                buf.write(data)
-            del data
-            return buf.getvalue()
-        else:
-            # Read until size bytes or \n or EOF seen, whichever comes first
-            buf.seek(0, 2)  # seek end
-            buf_len = buf.tell()
-            if buf_len >= size:
-                buf.seek(0)
-                rv = buf.read(size)
-                self._rbuffer = io.BytesIO()
-                self._rbuffer.write(buf.read())
-                return rv
-            self._rbuffer = io.BytesIO()  # reset _rbuf.  we consume it via buf.
-            while True:
-                try:
-                    data = self.recv(self.bufsize)
-                except socket.error as e:
-                    if e.args[0] == errno.EINTR:
-                        continue
-                    raise
-                if not data:
-                    break
-                left = size - buf_len
-                # did we just receive a newline?
-                nl = data.find(b'\n', 0, left)
-                if nl >= 0:
-                    nl += 1
-                    # save the excess data to _rbuf
-                    self._rbuffer.write(data[nl:])
-                    if buf_len:
-                        buf.write(data[:nl])
-                        break
-                    else:
-                        # Shortcut.  Avoid data copy through buf when returning
-                        # a substring of our first recv().
-                        return data[:nl]
-                n = len(data)
-                if n == size and not buf_len:
-                    # Shortcut.  Avoid data copy through buf when
-                    # returning exactly all of our first recv().
-                    return data
-                if n >= left:
-                    buf.write(data[:left])
-                    self._rbuffer.write(data[left:])
-                    break
-                buf.write(data)
-                buf_len += n
-                # assert buf_len == buf.tell()
-            return buf.getvalue()
-
-    def close(self):
-        if self._sock:
-            self._sock.close()
-
-    def __del__(self):
-        self.close()
-
     def dup(self):
         new = hxssocket()
         new.hxsServer = self.hxsServer
@@ -220,9 +129,6 @@ class hxssocket(object):
         new._rbuffer = self._rbuffer
         new.method = self.method
         return new
-
-    def settimeout(self, timeout):
-        self._sock.settimeout(timeout)
 
 if __name__ == '__main__':
     hxs = hxssocket('hxs://user:pass@127.0.0.1:80')
