6f2fd369f2be85010b23497d2b60aeb5ef5f41b7
v3aqb <sgzz.cj@gmail.com>
2016-07-09 23:28:42 +0800

    v4.15.3

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A anti-censorship HTTP proxy with builtin shadowsocks support.
 
-Current Version: 4.15.2
+Current Version: 4.15.3
 
 Tested on Windows 7 and openSUSE.
 
--- fgfw-lite/fgfw-lite.py
+++ fgfw-lite/fgfw-lite.py
@@ -97,7 +97,7 @@ except ImportError:
     def on_finish(hdlr):
         pass
 
-__version__ = '4.15.2'
+__version__ = '4.15.3'
 
 if sys.platform.startswith('win'):
     PYTHON2 = '"./Python27/python27.exe"'
