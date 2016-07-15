3539e7ad4e1ae79eedb5e63341f8e5feb24c5190
v3aqb <sgzz.cj@gmail.com>
2016-07-15 22:48:11 +0800

    gfwlist_url outdated

--- fgfw-lite/fgfw-lite.py
+++ fgfw-lite/fgfw-lite.py
@@ -979,6 +979,9 @@ def update(conf, logger, auto=False):
     if auto and not conf.userconf.dgetbool('FGFW_Lite', 'autoupdate'):
         return
     gfwlist_url = conf.userconf.dget('fgfwproxy', 'gfwlist_url', 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt')
+    if 'googlecode' in gfwlist_url:
+        conf.userconf.set('fgfwproxy', 'gfwlist_url', 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt')
+        conf.confsave()
 
     filelist = [(gfwlist_url, './fgfw-lite/gfwlist.txt'), ]
 
