01ad1735b1a1bb04f3ebf627185c133edb2d7a40
v3aqb <sgzz.cj@gmail.com>
2018-08-02 09:54:49 +0800

    v4.21.3

--- fgfw-lite/fgfw-lite.py
+++ fgfw-lite/fgfw-lite.py
@@ -97,7 +97,7 @@ except ImportError:
     def on_finish(hdlr):
         pass
 
-__version__ = '4.21.2'
+__version__ = '4.21.3'
 
 NetWorkIOError = (IOError, OSError, BufEmptyError, InvalidTag)
 DEFAULT_TIMEOUT = 5
